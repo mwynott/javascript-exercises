4c73dc98120d700457eb09932647cc2194e18a6a
mwynott <mwynott1989@gmail.com>
2026-01-12 13:12:20 -0500

    Use for loop with math min and max to return sum that includes start and end values

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,6 +1,14 @@
-const sumAll = function() {
+const sumAll = function(min, max) {
+   let sum = 0; 
+   const low = Math.min(min, max);
+   const high = Math.max(min, max);
 
+   for (let i = low; i <= high; i++) {
+    sum += i;
+    }
+   return sum;
 };
+
 
 // Do not edit below this line
 module.exports = sumAll;
