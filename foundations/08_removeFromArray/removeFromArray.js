f1fb8d78c9e1f2b965df6b72603ec4c8c85872ca
mwynott <mwynott1989@gmail.com>
2026-01-12 12:34:38 -0500

    Construct function to remove items from array.

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,10 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...items) {
+    const newArray = [];
+    array.forEach(part => {
+        if (!items.includes(part))
+            newArray.push(part);
+    });
+    return newArray;
 };
 
 // Do not edit below this line
