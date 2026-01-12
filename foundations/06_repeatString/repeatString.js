2c8834875260dcc03a0d8127ef37ce7fa68d204f
mwynott <mwynott1989@gmail.com>
2026-01-12 10:45:28 -0500

    Construct loop to repeat word three times

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,6 +1,22 @@
-const repeatString = function() {
+const repeatString = function (word, times) {
+    string = "";
+    word = 'hey';
+    times = 3;
+    for(let i = 0; i < times; i++) {
+      string += word;
+    }
+    return (string);
+}
 
-};
+
+
+
+
+
+
+
+
+
 
 // Do not edit below this line
 module.exports = repeatString;
